91dd8213a3b957eb01be65ddef1acfe842251a8b
VikVenkat <vik.venkat@gmail.com>
2018-01-21 14:17:10 -0500

    CSV dumps from browser

--- app/controllers/builders_controller.rb
+++ app/controllers/builders_controller.rb
@@ -5,6 +5,12 @@ class BuildersController < ApplicationController
   # GET /builders.json
   def index
     @builders = Builder.all
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data @builders.to_csv, filename: "builders-#{Date.today}.csv" }
+    end
+
   end
 
   # GET /builders/1
--- app/models/builder.rb
+++ app/models/builder.rb
@@ -3,20 +3,6 @@ class Builder < ActiveRecord::Base
   require 'scraped_list'
   require 'scraped_item'
 
-#attr_accessible :company, :phone, :email, :location, :typical, :url, :type, :contact_name
-
-  # def self.dedupe # Not quite working
-  #   # returns only duplicates in the form of [[name1, year1, trim1], [name2, year2, trim2],...]
-  #   duplicate_row_values = Builder.select('url, count(*)').group('url').having('count(*) > 1').pluck(:url)
-  #   @count = duplicate_row_values.count
-  #
-  #   # load the duplicates and order however you wantm and then destroy all but one
-  #   duplicate_row_values.each do |url|
-  #     Builder.where(url: url).order(id: :desc)[1..-1].map(&:destroy)
-  #   end
-  #   puts "Builder Table Deduped #{@count}x"
-  # end
-
   def self.dedupe
     @counter = 0
     ::Builder.all.each do |loc| #1
@@ -28,6 +14,19 @@ class Builder < ActiveRecord::Base
     end #do 1
     puts "============================"
     puts "Cleaned up #{@counter} duplicates"
+  end
+
+  def self.to_csv
+    attributes = %w{id contact_name company phone location site}
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      all.each do |item|
+        csv << attributes.map{ |attr| item.send(attr) }
+      end
+    end
+
   end
 
 
